c29296fb3e607ff5a3f8eceef7016b1abce1e38a
Pere <pere.solaclaver@gmail.com>
2019-11-18 16:55:03 +0000

    get stories endoint with summy data

--- stories/storiesRouter.js
+++ stories/storiesRouter.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const Stories = require("./storiesModels");
 
-router.get("/", (req, res) => {
+// DUMMY TESTING ENDPOINT
+router.get("/test", (req, res) => {
   res.status(200).json({ message: `Hello from dummy endpoint` });
 });
 
+// STORIES ENDPOINTS
+router.get("/", (req, res) => {
+  Stories.getAll()
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ message: `Could not retrieve stories: ${error.message}` });
+    });
+});
 module.exports = router;
