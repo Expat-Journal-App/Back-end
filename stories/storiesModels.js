1e3f9036f051f9f7ea438697a030a309a7650cc8
Pere <pere.solaclaver@gmail.com>
2019-11-19 14:39:11 +0000

    Get stories by id endpoint

--- stories/storiesModels.js
+++ stories/storiesModels.js
@@ -1,7 +1,8 @@
 const db = require("../database/db-config");
 
 module.exports = {
-  getAll
+  getAll,
+  getStoriesById
 };
 
 function getAll() {
@@ -20,4 +21,24 @@ function getAll() {
       "P.url",
       "P.description"
     );
+}
+
+function getStoriesById(id) {
+  return db("locationsStories as LS")
+    .join("stories as S", "LS.story_id", "S.id")
+    .join("locations as L", "LS.location_id", "L.id")
+    .join("photos as P", "P.story_id", "S.id")
+    .select(
+      "S.id",
+      "S.title",
+      "S.story",
+      "S.date_trip",
+      "S.created_at",
+      "L.city",
+      "L.country",
+      "P.url",
+      "P.description"
+    )
+    .where("S.id", id)
+    .first();
 }
--- stories/storiesRouter.js
+++ stories/storiesRouter.js
@@ -7,7 +7,7 @@ router.get("/test", (req, res) => {
   res.status(200).json({ message: `Hello from dummy endpoint` });
 });
 
-// STORIES ENDPOINTS
+// GET ALL STORIES ENDPOINTS
 router.get("/", (req, res) => {
   Stories.getAll()
     .then(data => {
@@ -17,6 +17,21 @@ router.get("/", (req, res) => {
       res
         .status(500)
         .json({ message: `Could not retrieve stories: ${error.message}` });
+    });
+});
+
+// GET STORY BY ID ENDPOINT
+router.get("/:id", (req, res) => {
+  Stories.getStoriesById(req.params.id)
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({
+          message: `There was an error fetching story ${id}: ${error.message}`
+        });
     });
 });
 module.exports = router;
