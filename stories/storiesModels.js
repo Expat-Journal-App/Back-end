ada6a88bca070178161845255df5293d46ef77d7
Pere <pere.solaclaver@gmail.com>
2019-11-19 11:02:04 +0000

    Retrieve photo url and description

--- stories/storiesModels.js
+++ stories/storiesModels.js
@@ -8,5 +8,6 @@ function getAll() {
   return db("locationsStories as LS")
     .join("stories as S", "LS.story_id", "S.id")
     .join("locations as L", "LS.location_id", "L.id")
-    .select("S.id", "S.title", "L.city", "L.country");
+    .join("photos as P", "P.story_id", "S.id")
+    .select("S.id", "S.title", "L.city", "L.country", "P.url", "P.description");
 }
