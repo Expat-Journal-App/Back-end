5bd5c08c6a7c05906c9a2a15b7aa0dff8ed8d45a
Pere <pere.solaclaver@gmail.com>
2019-11-19 09:54:21 +0000

    Add pool property to knex file

--- knexfile.js
+++ knexfile.js
@@ -12,6 +12,12 @@ module.exports = {
     },
     seeds: {
       directory: "./database/seeds"
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run("PRAGMA foreign_keys = ON", done); // turn on FK enforcement
+      }
     }
   },
 
