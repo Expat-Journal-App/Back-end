fe97150cbd0b39038b2f15a91e5beabba305e250
Pere <pere.solaclaver@gmail.com>
2019-11-19 15:37:12 +0000

    MOve middleware to own file

--- stories/storiesRouter.js
+++ stories/storiesRouter.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const Stories = require("./storiesModels");
 
+// IMPORT MIDDLEWARE
+
+const middleware = require("./helpers/middleware");
+
 // DUMMY TESTING ENDPOINT
 router.get("/test", (req, res) => {
   res.status(200).json({ message: `Hello from dummy endpoint` });
@@ -21,23 +25,8 @@ router.get("/", (req, res) => {
 });
 
 // GET STORY BY ID ENDPOINT
-router.get("/:id", checkValidtyId, (req, res) => {
+router.get("/:id", middleware.checkValidtyId, (req, res) => {
   res.status(200).json(req.data);
 });
 
-function checkValidtyId(req, res, next) {
-  const { id } = req.params;
-  Stories.getStoriesById(id)
-    .then(data => {
-      if (!data) {
-        res.status(404).json({ message: `Story ${id} could not be found` });
-      } else {
-        req.data = data;
-        next();
-      }
-    })
-    .catch(error => {
-      console.log(error);
-    });
-}
 module.exports = router;
