e9c0c485047426aeb58897d342935a748068c9c9
Pere <pere.solaclaver@gmail.com>
2019-11-21 22:08:22 +0000

    Restricted middleware

---
+++ users/helpers/middleware.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = {
+  restricted
+};
+
+function restricted(req, res, next) {
+  const token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ message: err.message });
+      } else {
+        req.decodedToken = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ message: `No credentials provided` });
+  }
+}
