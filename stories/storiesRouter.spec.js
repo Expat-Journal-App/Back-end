bea1f87c51ff22792e8731c666be13de093982cd
Pere <pere.solaclaver@gmail.com>
2019-11-22 01:20:21 +0000

    further testing

--- stories/storiesRouter.spec.js
+++ stories/storiesRouter.spec.js
@@ -27,5 +27,39 @@ describe("/api/stories", () => {
           expect(response.status).toBe(expectedStatusCode);
         });
     });
+    it("should return a JSON object when returning stories", () => {
+      request(server)
+        .get("/api/stories/")
+        .set("Authorization", token)
+        .then(response => {
+          expect(response.type).toEqual("application/json");
+        });
+    });
+    it("first object in arrat returns expected title", () => {
+      request(server)
+        .get("/api/stories/")
+        .set("Authorization", token)
+        .then(response => {
+          //   console.log(response.body[0]);
+          expect(response.body[0]).toMatchObject({
+            title: "A trip to Kiney Lake"
+          });
+        });
+    });
+    it("the objects have the expected properties", () => {
+      request(server)
+        .get("/api/stories/")
+        .set("Authorization", token)
+        .then(response => {
+          expect(response.body[0]).toHaveProperty("id");
+          expect(response.body[0]).toHaveProperty("title");
+          expect(response.body[0]).toHaveProperty("story");
+          expect(response.body[0]).toHaveProperty("date_trip");
+          expect(response.body[0]).toHaveProperty("city");
+          expect(response.body[0]).toHaveProperty("country");
+          expect(response.body[0]).toHaveProperty("url");
+          expect(response.body[0]).toHaveProperty("description");
+        });
+    });
   });
 });
